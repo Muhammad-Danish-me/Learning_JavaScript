5d8551367346f8746e0b26c147911eac00f9875e
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-16 16:43:17 +0500

    Class 5 ( Date and Time)

---
+++ Class_05/app.js
@@ -0,0 +1,11 @@
+// Date and Time
+let myDate = new Date();
+console.log(myDate);
+console.log(`Date ${myDate.getDate()}`); // Get Current System Date (1 - 31)
+console.log(`Day ${myDate.getDay()}`); // Get Current Day (0 is Sunday)
+console.log(`Month ${myDate.getMonth()}`); // Get Current Month (0 is January)
+console.log(`Year ${myDate.getFullYear()}`); // Get Current Year
+console.log(`Hour ${myDate.getHours()}`); // Get Current Hour (0 to 23)
+console.log(`Milliseconds ${myDate.getMilliseconds()}`); // Get Milliseconds (0 - 59)
+console.log(`Minutes ${myDate.getMinutes()}`); // Get Current minutes (0 - 59)
+console.log(`Time ${myDate.getTime()}`); // Milliseconds since midnight, January 1, 1970
