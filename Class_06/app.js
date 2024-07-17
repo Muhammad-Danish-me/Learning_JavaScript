0da15a2b8fb005f0749ce27f9d8381a565cfe971
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-17 20:18:22 +0500

    Update app.js

--- Class_06/app.js
+++ Class_06/app.js
@@ -44,3 +44,12 @@ let nestedArr = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10], 11];
 console.log(nestedArr);
 let allArr = nestedArr.flat(Infinity);
 console.log(allArr);
+
+console.log(Array.isArray("Danish"));
+console.log(Array.from("Danish")); // Create an Array from an iterable object
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // Returns a new Array from a set of elements
