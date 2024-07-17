d2a0888dce708c1265396e0663610864240953d7
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-17 19:46:37 +0500

    Class_06 (Array)

---
+++ Class_06/app.js
@@ -0,0 +1,29 @@
+// Array
+let myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+console.log(myArr[0]); // 1
+
+// Methods
+myArr.push(6); // Push Add one or more Element at the end of an array
+console.log("Push", myArr);
+
+myArr.pop(); // Pop remove one element at the end of an array
+console.log("Pop", myArr);
+
+myArr.unshift(0); // Unshift Add one or more Element at the Start of an array
+console.log("Unshift", myArr);
+
+myArr.shift(); // shift Remove one element at the start of an array
+console.log("Shift", myArr);
+
+console.log(myArr.includes(1)); // True
+
+// Slice And Splice
+let myNewArr = [0, 1, 2, 3, 4, 5];
+console.log(myNewArr);
+console.log("Slice", myNewArr.slice(0, 3)); // Slice Return a copy of section of an array.
+console.log(myNewArr);
+console.log(myNewArr.splice(0, 3)); // Spilce Impact Orignal Array
+console.log(myNewArr);
+
+console.log(myNewArr.join()); // Join Convert Array Into String
