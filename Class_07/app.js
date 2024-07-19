f6353a48e309690876cf775e277ebf3c10b15847
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-20 00:21:34 +0500

    Update app.js

--- Class_07/app.js
+++ Class_07/app.js
@@ -31,7 +31,16 @@ const students = {
     firstName: "Muhammad",
     lastName: "Danish",
   },
-  course: "WMA"
+  course: "WMA",
 };
-console.log(students.studentFullName.firstName)
-console.log(students.studentFullName.lastName)
+console.log(students.studentFullName.firstName);
+console.log(students.studentFullName.lastName);
+
+const obj1 = { 1: "a", 2: "b", 3: "c" };
+const obj2 = { 4: "d", 5: "e", 6: "f" };
+
+// const obj3 = { obj1, obj2 };
+// const obj3 = Object.assign(obj1,obj2)
+const obj3 = { ...obj1, ...obj2 }; // Spread Operator
+
+console.log(obj3);
