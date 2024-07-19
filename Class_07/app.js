52f9672fd098e25442087e420608eaaeaafd70aa
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-20 00:37:37 +0500

    Update app.js

--- Class_07/app.js
+++ Class_07/app.js
@@ -116,9 +116,9 @@ console.log(mobilephones);
 
 const car = { type: "Fiat", model: "500", color: "white" };
 // console.log(car.color)
-console.log(Object.keys(car));
-console.log(Object.values(car));
-console.log(Object.entries(car));
+// console.log(Object.keys(car));
+// console.log(Object.values(car));
+// console.log(Object.entries(car));
 
 // console.log(car.hasOwnProperty("color"));
 // console.log(car.hasOwnProperty("horsePower"));
