5672555bfcdfd2c98d3e7adfc4680b3df1052377
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-20 00:12:56 +0500

    Update app.js

--- Class_07/app.js
+++ Class_07/app.js
@@ -11,7 +11,27 @@ console.log(userData);
 console.log(userData.name);
 userData.email = "danish@yahoo.com";
 console.log(userData);
-Object.freeze(userData);
+// Object.freeze(userData);
 userData.name = "Hammad";
 console.log(userData);
-console.log(userData["full name"])
+console.log(userData["full name"]);
+
+userData.greeting = function () {
+  console.log("Hello Js User");
+};
+userData.greetingTwo = function () {
+  console.log(`Hello Js User ${this.name}`);
+};
+console.log(userData.greeting());
+console.log(userData.greetingTwo());
+
+const students = {
+  id: "123abc",
+  studentFullName: {
+    firstName: "Muhammad",
+    lastName: "Danish",
+  },
+  course: "WMA"
+};
+console.log(students.studentFullName.firstName)
+console.log(students.studentFullName.lastName)
