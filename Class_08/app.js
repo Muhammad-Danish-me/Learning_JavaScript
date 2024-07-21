802a71d062eb19de6b94913a6d59cef85dcbe35a
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-21 20:13:04 +0500

    Class 08 (Functions)

---
+++ Class_08/app.js
@@ -0,0 +1,10 @@
+// Functions
+function sayMyName() {
+  console.log("D");
+  console.log("A");
+  console.log("N");
+  console.log("I");
+  console.log("S");
+  console.log("H");
+}
+sayMyName(); // Call the function
