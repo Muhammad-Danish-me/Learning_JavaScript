c1ed0b9ffa10baeb35fefae79146d3c98b7cf54a
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-22 20:22:33 +0500

    Scoops , Hoisting

--- Class_09/app.js
+++ Class_09/app.js
@@ -7,10 +7,35 @@ if (true) {
   var a = 100;
   let b = 200;
   const c = 300;
-  console.log("Inner", a);
-  console.log("Inner", b);
-  console.log("Inner", c);
+  // console.log("Inner", a);
+  // console.log("Inner", b);
+  // console.log("Inner", c);
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+  const userName = "Danish";
+  function two() {
+    const website = "Youtube";
+    console.log(userName);
+  }
+  // console.log(website);
+  two();
+}
+// console.log(userName)
+one();
+// ******************  Hoisting Concept
+
+console.log(addOne(5));
+function addOne(num) {
+  return num + 1;
+}
+
+// console.log(addTwo(5)) Error, Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+  return num + 2;
+};
+
+console.log(addTwo(5));
