4e748070f0fd112f38b8be47a211837caf207f81
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-22 21:02:09 +0500

    This Concept

--- Class_10/app.js
+++ Class_10/app.js
@@ -1,1 +1,11 @@
 // This and Arrow functions
+const user = {
+  userName: "Danish",
+  price: 199,
+  welcomeMessage: function () {
+    console.log(`${this.userName}, Welcome to Website `);
+  },
+};
+user.welcomeMessage();
+user.userName = "Hammad";
+user.welcomeMessage();
