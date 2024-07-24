f234d7fdd4cf417d618857932d2c4312c3fae577
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-24 19:28:51 +0500

    Update app.js

--- Class_10/app.js
+++ Class_10/app.js
@@ -28,3 +28,8 @@ const arrowFunc = (num1, num2) => {
   return num1 + num2;
 };
 console.log(arrowFunc(2, 2));
+
+// const arrowFunc2 = (num1, num2) => (num1 + num2);
+const arrowFunc2 = (num1, num2) => num1 + num2;
+
+console.log(arrowFunc2(2, 5));
