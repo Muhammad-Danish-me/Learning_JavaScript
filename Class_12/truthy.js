5f11e5d7898c2b85ea11b6de59ab5f093fedfe4c
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-25 16:55:59 +0500

    Update truthy.js

--- Class_12/truthy.js
+++ Class_12/truthy.js
@@ -33,4 +33,4 @@ console.log(val1);
 // condition ? true : false
 
 const iceTeaPrice = 100;
-iceTeaPrice >= 80 ? console.log(`less than 80`) : console.log(`More than 80`);
+iceTeaPrice <= 80 ? console.log(`less than 80`) : console.log(`More than 80`);
