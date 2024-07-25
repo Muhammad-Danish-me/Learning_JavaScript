a88c2ba0cdcd1b50aae54ad5b3a5d1cd6fdb0666
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-25 14:52:48 +0500

    Update app.js

--- Class_12/app.js
+++ Class_12/app.js
@@ -6,3 +6,9 @@ if (temperature < 50) {
 } else {
   console.log(`Temperature is Greater than 50`);
 }
+
+// Implicit Scoop
+const balance = 2000;
+if (balance > 500) console.log("test"), console.log("Test 2");
+
+
