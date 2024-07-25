37b0e87becdae1a43e7f8560a49b4275c440a41f
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-25 16:36:57 +0500

    Update truthy.js

--- Class_12/truthy.js
+++ Class_12/truthy.js
@@ -11,3 +11,13 @@ if (userEmail) {
 } else {
   console.log(`Don't Have user email`);
 }
+
+const emptyArray = [];
+if (emptyArray.length === 0) {
+  console.log(`Array is empty`);
+}
+
+const emptyObj = {};
+if (Object.keys(emptyObj).length === 0) {
+  console.log(`object is empty`);
+}
