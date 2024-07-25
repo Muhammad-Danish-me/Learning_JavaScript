0b07f96ea6bb0e063b29fa1b49ca6906e69a4f00
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-25 14:42:42 +0500

    Class 12 (Control Flow)
    Conditional Statements

---
+++ Class_12/app.js
@@ -0,0 +1,8 @@
+// control Flow in JavaScript
+//  IF
+const temperature = 41;
+if (temperature < 50) {
+  console.log(`Temperature is Less than 50`);
+} else {
+  console.log(`Temperature is Greater than 50`);
+}
