8bdd6d958ce54db3b7186967e89766265a5c751c
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-25 15:57:14 +0500

    Falsy and Truthy Value

---
+++ Class_12/truthy.js
@@ -0,0 +1,10 @@
+// Falsy Value
+// false, 0, -0, BigInt 0n, "", null, undefined, Nan
+
+const userEmail = "danish@gmail.com";
+
+if (userEmail) {
+  console.log(`Got user email`);
+} else {
+  console.log(`Don't Have user email`);
+}
