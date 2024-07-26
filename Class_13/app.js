eff1f6be935b790b6276fb4e104abe35fe4e6549
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-26 16:28:40 +0500

    Update app.js

--- Class_13/app.js
+++ Class_13/app.js
@@ -14,6 +14,7 @@ for (let i = 0; i <= 5; i++) {
   }
 }
 for (let i = 1; i <= 10; i++) {
+  console.log(`The Table is ${i}`);
   for (let j = 1; j <= 10; j++) {
     console.log(`${i} x ${j} = ${i * j}`);
   }
