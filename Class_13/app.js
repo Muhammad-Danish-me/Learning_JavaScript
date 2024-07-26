9a8b7d4fa6f8ac879cca766e5d6a5cde0f183513
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-26 16:24:52 +0500

    Update app.js

--- Class_13/app.js
+++ Class_13/app.js
@@ -13,3 +13,8 @@ for (let i = 0; i <= 5; i++) {
     console.log(`Inner Loop Value: ${j} and Outer loop ${i}`);
   }
 }
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+}
