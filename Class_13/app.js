ce064c3fadd904cb591705aa178af5790393ef59
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-26 16:41:41 +0500

    Update app.js

--- Class_13/app.js
+++ Class_13/app.js
@@ -25,3 +25,11 @@ for (let index = 0; index < myArray.length; index++) {
   const element = myArray[index];
   console.log(element);
 }
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    console.log(`Detected 5`);
+    break;
+  }
+  console.log(`Value of i is ${i}`);
+}
