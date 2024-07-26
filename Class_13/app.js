a308cf89d528f161240cbb7d0f5d304fbae92109
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-26 16:42:44 +0500

    Continue in for loop

--- Class_13/app.js
+++ Class_13/app.js
@@ -33,3 +33,11 @@ for (let i = 1; i <= 20; i++) {
   }
   console.log(`Value of i is ${i}`);
 }
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    console.log(`Detected 5`);
+    continue;
+  }
+  console.log(`Value of i is ${i}`);
+}
