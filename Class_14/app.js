d9764b76f4192568e39753020bdd4a666465fe9c
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-27 16:11:07 +0500

    For of loop

--- Class_14/app.js
+++ Class_14/app.js
@@ -5,3 +5,11 @@ const arr = [1, 2, 3, 4, 5];
 for (const num of arr) {
   console.log(num);
 }
+
+const greeting = "Hello World!";
+for (const greet of greeting) {
+  if (greet === " ") {
+    continue;
+  }
+  console.log(`The Char of ${greet}`);
+}
