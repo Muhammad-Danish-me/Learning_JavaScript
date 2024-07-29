92e59d8addd24a6128dd3ef53a902ea9f451d01f
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-29 20:22:44 +0500

    DOM

--- Class_15/app.js
+++ Class_15/app.js
@@ -1,1 +1,9 @@
 // Dom
+let h1 = document.getElementById("title");
+
+console.log(h1.id);
+console.log(h1.className);
+console.log(h1.getAttribute("id"));
+console.log(h1.setAttribute("class", "hello")); // it's overwrite class which is before assign
+console.log(h1);
+h1.style.backgroundColor = "green"
