fec1df858443e69cd3db3faf02af5920a8cb9b0b
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-29 20:49:31 +0500

    Selectors

--- Class_15/app.js
+++ Class_15/app.js
@@ -9,4 +9,6 @@ console.log(h1);
 h1.style.backgroundColor = "green";
 h1.style.borderRadius = "15px";
 h1.style.padding = "15px";
-console.log(h1.innerHTML);
+console.log("inner Text: >", h1.innerText); // it's give value which is display on browser
+console.log("Text Content: >", h1.textContent); // it's give value which is display browser or hide
+console.log("inner HTML: >", h1.innerHTML); // it's give  value with html tags if it's have
