7abf5878ad189288782cacf8334f7e7ffbf9d0d4
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-29 20:35:13 +0500

    DOM Selectors

--- Class_15/app.js
+++ Class_15/app.js
@@ -6,4 +6,6 @@ console.log(h1.className);
 console.log(h1.getAttribute("id"));
 console.log(h1.setAttribute("class", "hello")); // it's overwrite class which is before assign
 console.log(h1);
-h1.style.backgroundColor = "green"
+h1.style.backgroundColor = "green";
+h1.style.borderRadius = "15px";
+h1.style.padding = "15px";
