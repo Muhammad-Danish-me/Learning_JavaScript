f4b178b54b97b951a89503e611f749589e3c8dc2
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-31 00:43:21 +0500

    Create  Element (Div) by JavaScript DOM

---
+++ Class_16/app.js
@@ -0,0 +1,10 @@
+const myDiv = document.createElement("div");
+console.log(myDiv);
+
+myDiv.className = "main";
+myDiv.id = "box";
+myDiv.setAttribute("title", "Generated title");
+myDiv.style.backgroundColor = "yellow"
+myDiv.style.color = "black"
+myDiv.innerText = "Hello";
+
