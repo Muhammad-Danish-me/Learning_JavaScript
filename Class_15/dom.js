96afe1a252b1aa6a5ad18d34131984805e74aee7
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-30 20:48:32 +0500

    DOM with Loop

--- Class_15/dom.js
+++ Class_15/dom.js
@@ -4,3 +4,7 @@ console.log(parent.children);
 console.log(parent.children[0]);
 console.log(parent.children[2]);
 console.log(parent.children[2].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+  console.log(parent.children[i].innerHTML);
+}
