df8f004a02663fd9dbc800dbf535273c10970d78
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-31 00:48:16 +0500

    append child

--- Class_16/app.js
+++ Class_16/app.js
@@ -4,7 +4,12 @@ console.log(myDiv);
 myDiv.className = "main";
 myDiv.id = "box";
 myDiv.setAttribute("title", "Generated title");
-myDiv.style.backgroundColor = "yellow"
-myDiv.style.color = "black"
-myDiv.innerText = "Hello";
+myDiv.style.backgroundColor = "yellow";
+myDiv.style.color = "black";
+// myDiv.innerText = "Hello World";
+const addText = document.createTextNode("Muhammad Danish");
+myDiv.appendChild(addText);
+document.body.appendChild(myDiv);
 
+myDiv.style.fontSize = "30px";
+myDiv.style.fontWeight = "bold";
