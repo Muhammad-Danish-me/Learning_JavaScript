99260e0414c9a8a414fbe0204ea8b38cf9c46f20
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-05 20:23:44 +0500

    Basic Color Changer Project Complete

--- Class_17_Projects/1_colorChanger/app.js
+++ Class_17_Projects/1_colorChanger/app.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll(".button");
 console.log(buttons);
 const body = document.querySelector("body");
+const clear = document.querySelector("#clear");
 
 buttons.forEach((button) => {
   console.log(button);
@@ -8,8 +9,18 @@ buttons.forEach((button) => {
     // console.log(e.target);
     if (e.target.id == "grey") {
       body.style.backgroundColor = e.target.id;
-    }else if(e.target.id == ""){
-
+    }
+    if (e.target.id == "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id == "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id == "green") {
+      body.style.backgroundColor = e.target.id;
     }
   });
+});
+clear.addEventListener("click", () => {
+  body.style.backgroundColor = "white";
 });
