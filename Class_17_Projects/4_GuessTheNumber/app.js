1e10b603d6327b9437fe0539d4f54935da8b1a4c
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-07 19:57:28 +0500

    get Elements by query selectors

---
+++ Class_17_Projects/4_GuessTheNumber/app.js
@@ -0,0 +1,13 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const preGueses = document.querySelector(".guesses");
+const remainGuess = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numOfGuess = 1;
+let playGame = true;
