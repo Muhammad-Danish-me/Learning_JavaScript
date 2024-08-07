fcf09cb5ca37bfe061112abb8a756355bf8ec713
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-07 20:15:14 +0500

    get value user input

--- Class_17_Projects/4_GuessTheNumber/app.js
+++ Class_17_Projects/4_GuessTheNumber/app.js
@@ -11,3 +11,22 @@ const p = document.createElement("p");
 let prevGuess = [];
 let numOfGuess = 1;
 let playGame = true;
+if (playGame) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess) {}
+function checkGuess(guess) {}
+
+function displayGuess(guess) {}
+
+function displayMessage(message) {}
+
+function endGame() {}
+
+function newGame() {}
