97d3954a511fb7676ec934cfec6a8fb11d79c025
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-07 20:28:45 +0500

    Update app.js

--- Class_17_Projects/4_GuessTheNumber/app.js
+++ Class_17_Projects/4_GuessTheNumber/app.js
@@ -1,14 +1,14 @@
 const randomNumber = parseInt(Math.random() * 100 + 1);
 const submit = document.querySelector("#subt");
 const userInput = document.querySelector("#guessField");
-const preGueses = document.querySelector(".guesses");
+const guessSlot = document.querySelector(".guesses");
 const remainGuess = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
 const startOver = document.querySelector(".resultParas");
 
 const p = document.createElement("p");
 
-let prevGuess = [];
+let preGuess = [];
 let numOfGuess = 1;
 let playGame = true;
 if (playGame) {
@@ -20,7 +20,25 @@ if (playGame) {
   });
 }
 
-function validateGuess(guess) {}
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("Please Enter a Valid Number");
+  } else if (guess < 1) {
+    alert("Please Enter a Number More Than 1");
+  } else if (guess > 100) {
+    alert("Please Enter a Number less Than 100");
+  } else {
+    preGuess.push(guess);
+    if (numOfGuess === 11) {
+      displayGuess(guess);
+      displayMessage(`Game over, Random number was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
 function checkGuess(guess) {}
 
 function displayGuess(guess) {}
