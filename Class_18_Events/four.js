3c1389ead7cc4931d550a0ca386590b5bf245e12
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-09 17:02:30 +0500

    Make Basic Todo List

--- Class_18_Events/four.js
+++ Class_18_Events/four.js
@@ -1,1 +1,15 @@
-console.log("testing")
+let userInput = document.getElementById("input");
+let addTask = document.querySelector("#addTask");
+let tasks = document.querySelector(".tasks");
+let p = document.createElement("p");
+
+addTask.addEventListener("click", (e) => {
+  let userValue = userInput.value;
+  if (userValue === "") {
+    alert("Please Enter Task");
+  } else {
+    tasks.innerHTML += `<h3>${userValue} </h3>`;
+
+    userInput.value = "";
+  }
+});
