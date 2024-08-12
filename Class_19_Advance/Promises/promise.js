e5a674b70c77f72de841f55f9f0ec1e142f0a8da
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-12 22:51:39 +0500

    Promises

--- Class_19_Advance/Promises/promise.js
+++ Class_19_Advance/Promises/promise.js
@@ -8,3 +8,12 @@ const promiseOne = new Promise(function (resolve, reject) {
 promiseOne.then(() => {
   console.log("Promise Consumed");
 });
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async Task 2");
+    resolve();
+  }, 1000);
+}).then(() => {
+  console.log("Async 2 Resolved");
+});
