44f4118f606f9465bfd5099ce2dd94a97e0e9d0a
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-08-15 20:41:45 +0500

    Update app.js

--- Class_20_classes_oop/app.js
+++ Class_20_classes_oop/app.js
@@ -1,4 +1,4 @@
-const user = {
+const users = {
   username: "Danish",
   loginCount: 4,
   signedIn: true,
@@ -8,6 +8,22 @@ const user = {
     console.log(this.username);
   },
 };
-console.log(user);
-console.log(user.username);
-console.log(user.getUserDetails());
+console.log(users);
+console.log(users.username);
+console.log(users.getUserDetails());
+
+//
+
+function user(username, loginCount, isLogedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLogedIn = isLogedIn;
+
+  return this;
+}
+const user1 = new user("Danish", 4, true);
+const user2 = new user("hammad", 5, false);
+
+console.log(user1);
+console.log(user2);
+console.log(user1 instanceof user);
